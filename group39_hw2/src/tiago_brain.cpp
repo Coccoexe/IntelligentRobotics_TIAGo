94d92beea886aca79b7dad439ebb0413502218b6
Alessio Cocco <alessio.cocco@studenti.unipd.it>
2024-01-12 17:59:14 +0100

    approaching

--- group39_hw2/src/tiago_brain.cpp
+++ group39_hw2/src/tiago_brain.cpp
@@ -1,256 +1,215 @@
 #include <ros/ros.h>
-#include <tiago_iaslab_simulation/Objs.h>           // ids
-#include <group39_hw1/MoveAction.h>                 // move
+#include <tiago_iaslab_simulation/Objs.h>           // IDs
+#include <group39_hw1/MoveAction.h>                 // HW1 TIAGo movement
 #include <actionlib/client/simple_action_client.h>
 #include <actionlib/client/terminal_state.h>
-#include <control_msgs/PointHeadAction.h>           // move_head
-#include <group39_hw2/DetectAction.h>               // detect
-#include <group39_hw2/ManipulateAction.h>           // manipulate
+#include <control_msgs/PointHeadAction.h>           // Head movement
+#include <group39_hw2/DetectAction.h>               // Detection
+#include <group39_hw2/ManipulateAction.h>           // Manipulation
 
 #include <moveit/planning_scene_interface/planning_scene_interface.h>
 #include <moveit/move_group_interface/move_group_interface.h>
 
-struct Gr39_Coordinates
+#define DEBUG true
+
+class TiagoBrain
 {
-    float x, y, z;
+private:
+    struct Gr39_Coordinates { float x, y, z; };
+    struct Gr39_Task { Gr39_Coordinates pickup, delivery; };
+
+    /**
+     * @brief Perform movement, pickup and delivery tasks for a given object ID
+     * 
+     * @param id Object ID
+     * @throws std::runtime_error if object ID is not recognized (i.e. not 1, 2 or 3)
+     */
+    void gr39_task(int id);
+
+    /**
+     * @brief Move TIAGo to a given position
+     * 
+     * @param x X coordinate
+     * @param y Y coordinate
+     * @param z Z angle (in degrees)
+     * @throws std::runtime_error if movement server does not finish after 60 seconds
+     */
+    void gr39_move(float x, float y, float z);
+    
+    /**
+     * @brief Perform pickup task for a given object ID
+     * 
+     * @param id Object ID
+     * @throws std::runtime_error if no objects are detected, if object with given ID is not detected, or if actions take too long
+     */
+    void gr39_pickup(int id);
+
+    void gr39_deliver(int id);
+
+    /**
+     * @brief Move TIAGo's head to a given angle
+     * 
+     * @param a Angle
+     * @throws std::runtime_error if head movement server does not finish after 10 seconds
+     */
+    void gr39_move_head(float a);
+
+public:
+    /**
+     * @brief Construct a new Tiago Brain object and perform all tasks for all IDs received from human_objects_srv
+     *
+     * @note Handles exceptions WITHOUT crashing the routine (i.e. if an exception is thrown, the routine will continue with the next ID)
+     */
+    TiagoBrain();
 };
 
-/**
- * @brief Move TIAGo's head up or down, according to the angle
- * 
- * @param angle Angle in radians
- * @return true If the action was completed within the timeout
- */
-bool move_head(float angle)
+TiagoBrain::TiagoBrain()
 {
-    ROS_INFO("Moving head");
-    // Move head to look at object
-    actionlib::SimpleActionClient<control_msgs::PointHeadAction> ac("head_controller/point_head_action", true);
-    ROS_INFO("Waiting for action server to start.");
-    ac.waitForServer();
-    ROS_INFO("Action server started, sending goal.");
-    control_msgs::PointHeadGoal goal;
-    goal.target.header.frame_id = "base_link";
-    goal.target.point.x = 1.0;
-    goal.target.point.y = 0.0;
-    goal.target.point.z = angle;
-    goal.pointing_frame = "xtion_optical_frame";
-    //goal.pointing_axis.z = 0.0;
-    goal.max_velocity = 1.0;
-    goal.min_duration = ros::Duration(1.0);
-    ac.sendGoal(goal);
-
-    // Wait for the action to return
-    if (!ac.waitForResult(ros::Duration(10.0))) return false;
-    actionlib::SimpleClientGoalState state = ac.getState();
-    ROS_INFO("Action finished: %s", state.toString().c_str());
-    return true;
-}
-
-/**
- * @brief Move TIAGo to the given coordinates
- * 
- * @param x X coordinate
- * @param y Y coordinate
- * @param z Z angle
- * @return true If the action was completed within the timeout
- * @return false If the action was not completed within the timeout
- */
-bool gr39_move(float x, float y, float z)
-{
-    actionlib::SimpleActionClient<group39_hw1::MoveAction> ac("movement", true);
-    ROS_INFO("Waiting for action server to start.");
-    ac.waitForServer();
-    ROS_INFO("Action server started, sending goal.");
-    
-    group39_hw1::MoveGoal goal;
-    goal.x = x;
-    goal.y = y;
-    goal.theta = z;
-    ac.sendGoal(goal);
-    return ac.waitForResult(ros::Duration(60.0));
-}
-
-/**
- * @brief Pick up routine for the object with the given id
- * 
- * @param id Object id
- * @return int -1 if the object was not found or the action was not completed within the timeout, 0 otherwise
- */
-int gr39_pickup(int id)
-{
-    const float head_up = 1.0, head_down = 0.5;
-
-    // 0. Move head
-    if (!move_head(head_down))
-    {
-        ROS_INFO("TIAGo -> HEAD_DOWN movement did not finish before the time out.");
-        return -1;
-    }
-
-    // 1. Detection
-    actionlib::SimpleActionClient<group39_hw2::DetectAction> ac("detection", true);
-    ROS_INFO("Waiting for action server to start.");
-    ac.waitForServer();
-    ROS_INFO("Action server started, sending goal.");
-    group39_hw2::DetectGoal goal;
-    goal.ready = true;
-
-    ac.sendGoal(goal);
-    if (!ac.waitForResult(ros::Duration(10.0)))
-    {
-        ROS_INFO("Action did not finish before the time out.");
-        return -1;
-    }
-    actionlib::SimpleClientGoalState state = ac.getState();
-    ROS_INFO("Action finished: %s", state.toString().c_str());
-    group39_hw2::DetectResultConstPtr result = ac.getResult();
-
-    bool found = false;
-    for (const auto& obj : result->detectedObj)
-    {
-        if (obj.id == id) found = true;
-        //print pose
-        ROS_INFO("Object %d pose: x = %f, y = %f, z = %f", obj.id, obj.pose.position.x, obj.pose.position.y, obj.pose.position.z);
-    }
-    if (!found)
-    {
-        ROS_INFO("Object %d not found", id);
-        return -1;
-    }
-    
-    // 2. Manipulation
-    actionlib::SimpleActionClient<group39_hw2::ManipulateAction> ac2("manipulation", true);
-    ROS_INFO("Waiting for action server to start.");
-    ac2.waitForServer();
-    ROS_INFO("Action server started, sending goal.");
-    group39_hw2::ManipulateGoal goal2;
-    goal2.attach = true;
-    goal2.id = id;
-    goal2.detectedObj = result->detectedObj;
-    ac2.sendGoal(goal2);
-    if (!ac2.waitForResult(ros::Duration(20.0)))
-    {
-        ROS_INFO("Action did not finish before the time out.");
-        return -1;
-    }
-
-    // 3. Move head
-    if (!move_head(head_up))
-    {
-        ROS_INFO("TIAGo -> HEAD_UP movement did not finish before the time out.");
-        return -1;
-    }
-
-    return 0;
-}
-
-void gr39_deliver(int id)
-{   // TODO: implement
-    // 1. Place object on table
-    // 2. Open gripper
-    // 3. Detach object from gripper
-}
-
-int main (int argc, char **argv)
-{
-    // Constants
-    struct Gr39_Task
-    {
-        Gr39_Coordinates pickup, delivery;
-    };
-    const Gr39_Coordinates
-        COORD_BASE = {8.5, 0.5, -45},
-        COORD_G = {8.7, -4.2, -180};
-    const Gr39_Task TASK[] = {
-        {{8.9, -2.7, -180}, {0.0, 0.0, -90}},   // Blue
-        {{7.7, -4.1, 90}, {0.0, 0.0, -90}},     // Green
-        {{7.2, -1.9, -50}, {0.0, 0.0, -90}}};   // Red
-
-    // ROS Initialization
-    ros::init(argc, argv, "tiago_brain");
-
-    // Get ids
+    // Get objects IDs from human_objects_srv
     ros::NodeHandle nh;
     ros::ServiceClient client = nh.serviceClient<tiago_iaslab_simulation::Objs>("human_objects_srv");
     tiago_iaslab_simulation::Objs srv;
     srv.request.ready = true;
     srv.request.all_objs = true;
-    if (!client.call(srv))
+    if (!client.call(srv)) { ROS_ERROR("ERROR | Failed to call service human_objects_srv"); return; }
+
+    // Process objects
+    for (int id : srv.response.ids)
     {
-        ROS_ERROR("Failed to call service human_objects_srv");
-        return -1;
+        ROS_INFO("\nProcessing object %d ..", id);
+        try { gr39_task(id); }
+        catch (const std::exception& e) { ROS_ERROR("ERROR | %s", e.what()); }
+        if (DEBUG) break;
     }
-    ROS_INFO("Ready to start");
-    ROS_INFO("Object ID: %d", srv.response.ids[0]);
-    ROS_INFO("Object ID: %d", srv.response.ids[1]);
-    ROS_INFO("Object ID: %d", srv.response.ids[2]);
+}
 
-    // Perform tasks for each object id received
-    for (int i = 0; i < srv.response.ids.size(); i++)
+void TiagoBrain::gr39_task(int id)
+{
+    if (id < 1 || id > 3) throw std::runtime_error("Object ID not recognized");
+
+    // Constants
+    const struct { float x, y; } BASE = {8.5, 0.5};
+    const Gr39_Task TASKS[3] =
     {
-        int id = srv.response.ids[i];
+        { {8.15, -2.1, -90}, {0.0, 0.0, -90} }, //{ {8.9, -2.7, -180}, {0.0, 0.0, -90} }, // Blue
+        { {7.8, -4, 90},   {0.0, 0.0, -90} }, //{ {7.7, -4.1, 90},   {0.0, 0.0, -90} }, // Green
+        { {7.45, -2, -90},  {0.0, 0.0, -90} }  //{ {7.2, -1.9, -50},  {0.0, 0.0, -90} }  // Red
+    };
 
-        // 1. Move to COORD_BASE
-        if (!gr39_move(COORD_BASE.x, COORD_BASE.y, COORD_BASE.z))
-        {
-            ROS_INFO("TIAGo -> BASE did not finish before the time out.");
-            return -1;
-        }
+    // Position in front of object
+    gr39_move(BASE.x, BASE.y, -90);
+    if (id == 2) gr39_move(8.7, -4.2, -180); // Green is special
+    gr39_move(TASKS[id - 1].pickup.x, TASKS[id - 1].pickup.y, TASKS[id - 1].pickup.z);
+    
+    // Pick up object
+    gr39_pickup(id);
 
-        // 1.opt. Move to COORD_G
-        if (id == 2)
-        {
-            if (!gr39_move(COORD_G.x, COORD_G.y, COORD_G.z))
-            {
-                ROS_INFO("TIAGo (Green) -> optional BASE did not finish before the time out.");
-                return -1;
-            }
-        }
+    // Position in front of delivery
+    if (id == 2) gr39_move(8.7, -4.2, -270); // Green is special pt.2
+    gr39_move(BASE.x, BASE.y, 0);
+    gr39_move(TASKS[id - 1].delivery.x, TASKS[id - 1].delivery.y, TASKS[id - 1].delivery.z);
 
-        // 2. Move to object id's pickup coordinates
-        if (!gr39_move(TASK[id - 1].pickup.x, TASK[id - 1].pickup.y, TASK[id - 1].pickup.z))
-        {
-            ROS_INFO("TIAGo -> PICKUP point did not finish before the time out.");
-            return -1;
-        }
+    // Deliver object
+    gr39_deliver(id);
+}
 
-        // 3. Pick up object
-        if (gr39_pickup(id) == -1)
-        {
-            ROS_INFO("TIAGo -> PICKUP failed.");
-            return -1;
-        }
-        break;
+void TiagoBrain::gr39_move(float x, float y, float z)
+{
+    actionlib::SimpleActionClient<group39_hw1::MoveAction> ac("movement", true);
+    ROS_INFO("START  | Waiting for movement server to start.");
+    ac.waitForServer();
+    ROS_INFO("DONE   | Movement server started, sending goal.");
 
-        // 4.opt. Move to COORD_G
-        if (id == 2)
-        {
-            if (!gr39_move(COORD_G.x, COORD_G.y, COORD_G.z))
-            {
-                ROS_INFO("TIAGo (Green) -> optional BASE did not finish before the time out.");
-                return -1;
-            }
-        }
+    // Setting goal
+    group39_hw1::MoveGoal goal;
+    goal.x = x;
+    goal.y = y;
+    goal.theta = z;
+    ac.sendGoal(goal);
 
-        // 4. Move to COORD_BASE
-        if (!gr39_move(COORD_BASE.x, COORD_BASE.y, COORD_BASE.z))
-        {
-            ROS_INFO("TIAGo -> BASE did not finish before the time out.");
-            return -1;
-        }
-        
-        // 5. Move to delivery
-        if (!gr39_move(TASK[id - 1].delivery.x, TASK[id - 1].delivery.y, TASK[id - 1].delivery.z))
-        {
-            ROS_INFO("TIAGo -> DELIVERY point did not finish before the time out.");
-            return -1;
-        }
-        
-        // 6. Place object
-        gr39_deliver(id);
-        
-        // DEBUG only 1 object
-        break;
-    }
+    // Waiting for result
+    if (!ac.waitForResult(ros::Duration(60.0))) throw std::runtime_error("Movement server did not finish after 60 seconds");
+    actionlib::SimpleClientGoalState state = ac.getState();
+    ROS_INFO("STATUS | Movement server finished with state %s", state.toString().c_str());
+}
+
+void TiagoBrain::gr39_pickup(int id)
+{
+    // Look down at the table
+    const float HEAD_UP = 1.0, HEAD_DOWN = 0.5;
+    gr39_move_head(HEAD_DOWN);
+
+    // Detect objects
+    actionlib::SimpleActionClient<group39_hw2::DetectAction> ac("detection", true);
+    ROS_INFO("START  | Waiting for detection server to start.");
+    ac.waitForServer();
+    ROS_INFO("DONE   | Detection server started, sending goal.");
+
+    // Setting goal
+    group39_hw2::DetectGoal goal;
+    goal.ready = true;
+    ac.sendGoal(goal);
+    if (!ac.waitForResult(ros::Duration(10.0))) throw std::runtime_error("Detection server did not finish after 10 seconds");
+    actionlib::SimpleClientGoalState state = ac.getState();
+    ROS_INFO("STATUS | Detection server finished with state %s", state.toString().c_str());
+
+    // Get and check results
+    group39_hw2::DetectResultConstPtr result = ac.getResult();
+    if (result->detectedObj.size() == 0) throw std::runtime_error("No objects detected");
+    bool found = false;
+    for (const auto& obj : result->detectedObj) if (obj.id == id) { found = true; break; }
+    if (!found) throw std::runtime_error("Object with given ID not detected");
+
+    // Manipulate object
+    actionlib::SimpleActionClient<group39_hw2::ManipulateAction> ac2("manipulation", true);
+    ROS_INFO("START  | Waiting for manipulation server to start.");
+    ac2.waitForServer();
+    ROS_INFO("DONE   | Manipulation server started, sending goal.");
+
+    // Setting goal
+    group39_hw2::ManipulateGoal goal2;
+    goal2.attach = true;
+    goal2.id = id;
+    goal2.detectedObj = result->detectedObj;
+    ac2.sendGoal(goal2);
+    if (!ac2.waitForResult(ros::Duration(20.0))) throw std::runtime_error("Manipulation server did not finish after 20 seconds");
+
+    // Move head up
+    gr39_move_head(HEAD_UP);
+}
+
+void TiagoBrain::gr39_deliver(int id)
+{   // TODO: Implement
+    return;
+}
+
+void TiagoBrain::gr39_move_head(float a)
+{
+    actionlib::SimpleActionClient<control_msgs::PointHeadAction> ac("head_controller/point_head_action", true);
+    ROS_INFO("START  | Waiting for head movement server to start.");
+    ac.waitForServer();
+    ROS_INFO("DONE   | Head movement server started, sending goal.");
+
+    // Setting goal
+    control_msgs::PointHeadGoal goal;
+    goal.target.header.frame_id = "base_link";
+    goal.target.point.x = 1.0;
+    goal.target.point.y = 0.0;
+    goal.target.point.z = a;
+    goal.pointing_frame = "xtion_optical_frame";
+    goal.max_velocity = 1.0;
+    goal.min_duration = ros::Duration(1.0);
+    ac.sendGoal(goal);
+
+    // Waiting for result
+    if (!ac.waitForResult(ros::Duration(10.0))) throw std::runtime_error("Head movement server did not finish after 10 seconds");
+    actionlib::SimpleClientGoalState state = ac.getState();
+    ROS_INFO("STATUS | Head movement server finished with state %s", state.toString().c_str());
+}
+
+int main(int argc, char** argv)
+{
+    ros::init(argc, argv, "brain");
+    TiagoBrain tb;
+    return 0;
 }
---
+++ group39_hw2/src/tiago_brain_old.cpp
@@ -0,0 +1,256 @@
+#include <ros/ros.h>
+#include <tiago_iaslab_simulation/Objs.h>           // ids
+#include <group39_hw1/MoveAction.h>                 // move
+#include <actionlib/client/simple_action_client.h>
+#include <actionlib/client/terminal_state.h>
+#include <control_msgs/PointHeadAction.h>           // move_head
+#include <group39_hw2/DetectAction.h>               // detect
+#include <group39_hw2/ManipulateAction.h>           // manipulate
+
+#include <moveit/planning_scene_interface/planning_scene_interface.h>
+#include <moveit/move_group_interface/move_group_interface.h>
+
+struct Gr39_Coordinates
+{
+    float x, y, z;
+};
+
+/**
+ * @brief Move TIAGo's head up or down, according to the angle
+ * 
+ * @param angle Angle in radians
+ * @return true If the action was completed within the timeout
+ */
+bool move_head(float angle)
+{
+    ROS_INFO("Moving head");
+    // Move head to look at object
+    actionlib::SimpleActionClient<control_msgs::PointHeadAction> ac("head_controller/point_head_action", true);
+    ROS_INFO("Waiting for action server to start.");
+    ac.waitForServer();
+    ROS_INFO("Action server started, sending goal.");
+    control_msgs::PointHeadGoal goal;
+    goal.target.header.frame_id = "base_link";
+    goal.target.point.x = 1.0;
+    goal.target.point.y = 0.0;
+    goal.target.point.z = angle;
+    goal.pointing_frame = "xtion_optical_frame";
+    //goal.pointing_axis.z = 0.0;
+    goal.max_velocity = 1.0;
+    goal.min_duration = ros::Duration(1.0);
+    ac.sendGoal(goal);
+
+    // Wait for the action to return
+    if (!ac.waitForResult(ros::Duration(10.0))) return false;
+    actionlib::SimpleClientGoalState state = ac.getState();
+    ROS_INFO("Action finished: %s", state.toString().c_str());
+    return true;
+}
+
+/**
+ * @brief Move TIAGo to the given coordinates
+ * 
+ * @param x X coordinate
+ * @param y Y coordinate
+ * @param z Z angle
+ * @return true If the action was completed within the timeout
+ * @return false If the action was not completed within the timeout
+ */
+bool gr39_move(float x, float y, float z)
+{
+    actionlib::SimpleActionClient<group39_hw1::MoveAction> ac("movement", true);
+    ROS_INFO("Waiting for action server to start.");
+    ac.waitForServer();
+    ROS_INFO("Action server started, sending goal.");
+    
+    group39_hw1::MoveGoal goal;
+    goal.x = x;
+    goal.y = y;
+    goal.theta = z;
+    ac.sendGoal(goal);
+    return ac.waitForResult(ros::Duration(60.0));
+}
+
+/**
+ * @brief Pick up routine for the object with the given id
+ * 
+ * @param id Object id
+ * @return int -1 if the object was not found or the action was not completed within the timeout, 0 otherwise
+ */
+int gr39_pickup(int id)
+{
+    const float head_up = 1.0, head_down = 0.5;
+
+    // 0. Move head
+    if (!move_head(head_down))
+    {
+        ROS_INFO("TIAGo -> HEAD_DOWN movement did not finish before the time out.");
+        return -1;
+    }
+
+    // 1. Detection
+    actionlib::SimpleActionClient<group39_hw2::DetectAction> ac("detection", true);
+    ROS_INFO("Waiting for action server to start.");
+    ac.waitForServer();
+    ROS_INFO("Action server started, sending goal.");
+    group39_hw2::DetectGoal goal;
+    goal.ready = true;
+
+    ac.sendGoal(goal);
+    if (!ac.waitForResult(ros::Duration(10.0)))
+    {
+        ROS_INFO("Action did not finish before the time out.");
+        return -1;
+    }
+    actionlib::SimpleClientGoalState state = ac.getState();
+    ROS_INFO("Action finished: %s", state.toString().c_str());
+    group39_hw2::DetectResultConstPtr result = ac.getResult();
+
+    bool found = false;
+    for (const auto& obj : result->detectedObj)
+    {
+        if (obj.id == id) found = true;
+        //print pose
+        ROS_INFO("Object %d pose: x = %f, y = %f, z = %f", obj.id, obj.pose.position.x, obj.pose.position.y, obj.pose.position.z);
+    }
+    if (!found)
+    {
+        ROS_INFO("Object %d not found", id);
+        return -1;
+    }
+    
+    // 2. Manipulation
+    actionlib::SimpleActionClient<group39_hw2::ManipulateAction> ac2("manipulation", true);
+    ROS_INFO("Waiting for action server to start.");
+    ac2.waitForServer();
+    ROS_INFO("Action server started, sending goal.");
+    group39_hw2::ManipulateGoal goal2;
+    goal2.attach = true;
+    goal2.id = id;
+    goal2.detectedObj = result->detectedObj;
+    ac2.sendGoal(goal2);
+    if (!ac2.waitForResult(ros::Duration(20.0)))
+    {
+        ROS_INFO("Action did not finish before the time out.");
+        return -1;
+    }
+
+    // 3. Move head
+    if (!move_head(head_up))
+    {
+        ROS_INFO("TIAGo -> HEAD_UP movement did not finish before the time out.");
+        return -1;
+    }
+
+    return 0;
+}
+
+void gr39_deliver(int id)
+{   // TODO: implement
+    // 1. Place object on table
+    // 2. Open gripper
+    // 3. Detach object from gripper
+}
+
+int main (int argc, char **argv)
+{
+    // Constants
+    struct Gr39_Task
+    {
+        Gr39_Coordinates pickup, delivery;
+    };
+    const Gr39_Coordinates
+        COORD_BASE = {8.5, 0.5, -45},
+        COORD_G = {8.7, -4.2, -180};
+    const Gr39_Task TASK[] = {
+        {{8.9, -2.7, -180}, {0.0, 0.0, -90}},   // Blue
+        {{7.7, -4.1, 90}, {0.0, 0.0, -90}},     // Green
+        {{7.2, -1.9, -50}, {0.0, 0.0, -90}}};   // Red
+
+    // ROS Initialization
+    ros::init(argc, argv, "tiago_brain");
+
+    // Get ids
+    ros::NodeHandle nh;
+    ros::ServiceClient client = nh.serviceClient<tiago_iaslab_simulation::Objs>("human_objects_srv");
+    tiago_iaslab_simulation::Objs srv;
+    srv.request.ready = true;
+    srv.request.all_objs = true;
+    if (!client.call(srv))
+    {
+        ROS_ERROR("Failed to call service human_objects_srv");
+        return -1;
+    }
+    ROS_INFO("Ready to start");
+    ROS_INFO("Object ID: %d", srv.response.ids[0]);
+    ROS_INFO("Object ID: %d", srv.response.ids[1]);
+    ROS_INFO("Object ID: %d", srv.response.ids[2]);
+
+    // Perform tasks for each object id received
+    for (int i = 0; i < srv.response.ids.size(); i++)
+    {
+        int id = srv.response.ids[i];
+
+        // 1. Move to COORD_BASE
+        if (!gr39_move(COORD_BASE.x, COORD_BASE.y, COORD_BASE.z))
+        {
+            ROS_INFO("TIAGo -> BASE did not finish before the time out.");
+            return -1;
+        }
+
+        // 1.opt. Move to COORD_G
+        if (id == 2)
+        {
+            if (!gr39_move(COORD_G.x, COORD_G.y, COORD_G.z))
+            {
+                ROS_INFO("TIAGo (Green) -> optional BASE did not finish before the time out.");
+                return -1;
+            }
+        }
+
+        // 2. Move to object id's pickup coordinates
+        if (!gr39_move(TASK[id - 1].pickup.x, TASK[id - 1].pickup.y, TASK[id - 1].pickup.z))
+        {
+            ROS_INFO("TIAGo -> PICKUP point did not finish before the time out.");
+            return -1;
+        }
+
+        // 3. Pick up object
+        if (gr39_pickup(id) == -1)
+        {
+            ROS_INFO("TIAGo -> PICKUP failed.");
+            return -1;
+        }
+        break;
+
+        // 4.opt. Move to COORD_G
+        if (id == 2)
+        {
+            if (!gr39_move(COORD_G.x, COORD_G.y, COORD_G.z))
+            {
+                ROS_INFO("TIAGo (Green) -> optional BASE did not finish before the time out.");
+                return -1;
+            }
+        }
+
+        // 4. Move to COORD_BASE
+        if (!gr39_move(COORD_BASE.x, COORD_BASE.y, COORD_BASE.z))
+        {
+            ROS_INFO("TIAGo -> BASE did not finish before the time out.");
+            return -1;
+        }
+        
+        // 5. Move to delivery
+        if (!gr39_move(TASK[id - 1].delivery.x, TASK[id - 1].delivery.y, TASK[id - 1].delivery.z))
+        {
+            ROS_INFO("TIAGo -> DELIVERY point did not finish before the time out.");
+            return -1;
+        }
+        
+        // 6. Place object
+        gr39_deliver(id);
+        
+        // DEBUG only 1 object
+        break;
+    }
+}
